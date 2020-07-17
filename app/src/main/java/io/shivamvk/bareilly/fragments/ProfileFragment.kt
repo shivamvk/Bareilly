afef90a0fcf02460d49bf1b57e7053aea184bbfe
Ashish Garg <ashishgarg1998@gmail.com>
2020-07-17 11:20:41 +0530

    added collapsing toolbar in profile

--- app/src/main/java/io/shivamvk/bareilly/fragments/ProfileFragment.kt
+++ app/src/main/java/io/shivamvk/bareilly/fragments/ProfileFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import io.shivamvk.bareilly.R
+
 
 class ProfileFragment : Fragment() {
     companion object {
@@ -24,5 +29,30 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        //val tv = view.findViewById<TextView>(R.id.tv1)
+        val collapsingToolbarLayout =
+            view.findViewById(R.id.collapsingToolbarLayout) as CollapsingToolbarLayout
+        val appBarLayout = view.findViewById(R.id.app_bar) as AppBarLayout
+        appBarLayout.addOnOffsetChangedListener(object : OnOffsetChangedListener {
+            var isShow = true
+            var scrollRange = -1
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                    //collapsingToolbarLayout.setScrimsShown(false,false)
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.title = "Title"
+                    collapsingToolbarLayout.contentScrim = resources.getDrawable(R.color.colorPrimary)
+                    collapsingToolbarLayout.setScrimsShown(true,false)
+                    isShow = true
+                } else if (isShow) {
+                    collapsingToolbarLayout.title = ""
+                    isShow = false
+                }
+            }
+        })
     }
 }
